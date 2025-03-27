6f7a4095be823d9c14f3c4abeaf106ab99af1c71
Pavel Tarasov <pavel44tarasov@yandex.ru>
2025-03-27 22:25:58 +0400

    Product Controller commit

--- Main/java/base/Controller/ProductController.java
+++ Main/java/base/Controller/ProductController.java
@@ -1,9 +1,67 @@
 package base.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import base.Entity.Product;
+import base.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllBy();
+        return products != null && !products.isEmpty()
+                ? new ResponseEntity<>(products, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable(name = "id") int id) {
+        Product product = productService.getProductByProductId(id);
+        return product != null
+                ? new ResponseEntity<>(product, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/categories/id/{id}")
+    public ResponseEntity<List<Product>> getProductsByCategoryId(@PathVariable(name = "id") int id) {
+        List<Product> products = productService.getProductsByCategoryId(id);
+        return products != null
+                ? new ResponseEntity<>(products, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/manufactures/id/{id}")
+    public ResponseEntity<List<Product>> getProductsByManufacturerId(@PathVariable(name = "id") int id) {
+        List<Product> products = productService.getProductsByManufacturerId(id);
+        return products != null
+                ? new ResponseEntity<>(products, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product save(@RequestBody Product product) {
+        productService.saveProduct(product);
+        return product;
+    }
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable(name = "id") int id) {
+        try {
+            productService.deleteProduct(productService.getProductByProductId(id));
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Категория успено удалена");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ошибка при удалении категории: " + e.getMessage());
+        }
+    }
+
 }

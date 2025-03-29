d3c8a3fc66438ab53206de47790150575ae3c2a2
Pavel Tarasov <pavel44tarasov@yandex.ru>
2025-03-30 01:36:07 +0400

    Create CategoryDTO commit

---
+++ Main/java/base/DTO/CategoryDTO.java
@@ -0,0 +1,31 @@
+package base.DTO;
+
+public class CategoryDTO {
+
+    private int id;
+    private String name;
+
+    public CategoryDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

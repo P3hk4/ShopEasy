964917d9c06b1302224355d0dad936afdfdad171
Pavel Tarasov <pavel44tarasov@yandex.ru>
2025-04-01 01:15:13 +0400

    Update ProductController methods commit

--- Main/java/base/Repository/ProductRepository.java
+++ Main/java/base/Repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package base.Repository;
 
+import base.DTO.ProductDTO;
 import base.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,10 +22,21 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findProductsByManufacturerId(int id);
 
-    List<Product> findProductsByCategoryIdAndAndManufacturerId(int categoryId, int manufacturerId);
-
     List<Product> findProductsByPriceIsBetween(int minPrice, int maxPrice);
 
     @Query("SELECT p.productId FROM Products p")
     List<Integer> findAllProductIds();
+
+    @Query("SELECT new base.DTO.ProductDTO(p.productId,p.categoryId,p.manufacturerId,p.name,p.description,p.price) FROM Products p")
+    List<ProductDTO> findAllProductDTO();
+
+    @Query("SELECT new base.DTO.ProductDTO(p.productId,p.categoryId,p.manufacturerId,p.name,p.description,p.price) FROM Products p WHERE p.productId = :id")
+    ProductDTO findProductDTOByProductId(@Param("id") int id);
+
+    @Query("SELECT new base.DTO.ProductDTO(p.productId,p.categoryId,p.manufacturerId,p.name,p.description,p.price) FROM Products p WHERE p.categoryId = :categoryId")
+    List<ProductDTO> findAllProductDTOByCategoryId(@Param("categoryId") int id);
+
+    @Query("SELECT new base.DTO.ProductDTO(p.productId,p.categoryId,p.manufacturerId,p.name,p.description,p.price) FROM Products p WHERE p.manufacturerId = :manufacturerId")
+    List<ProductDTO> findAllProductDTOByManufacturerId(@Param("manufacturerId") int id);
+
 }

d8cfb76021220a8f2a27bffc6f29232ec87aa4b3
Pavel Tarasov <pavel44tarasov@yandex.ru>
2025-04-01 01:14:57 +0400

    Update ManufacturerController methods commit

--- Main/java/base/Repository/ManufacturerRepository.java
+++ Main/java/base/Repository/ManufacturerRepository.java
@@ -1,8 +1,10 @@
 package base.Repository;
 
+import base.DTO.ManufacturerDTO;
 import base.Entity.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,4 +22,13 @@ public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long
 
     @Query("SELECT m.manufacturerId FROM Manufacturers m")
     List<Integer> findAllManufacturerIds();
+
+    @Query("SELECT new base.DTO.ManufacturerDTO(m.manufacturerId,m.country,m.name) FROM Manufacturers m")
+    List<ManufacturerDTO> findAllManufacturerDTO();
+
+    @Query("SELECT new base.DTO.ManufacturerDTO(m.manufacturerId,m.country,m.name) FROM Manufacturers m WHERE m.manufacturerId = :id")
+    ManufacturerDTO findManufacturerDTOById(@Param("id") int id);
+
+    @Query("SELECT new base.DTO.ManufacturerDTO(m.manufacturerId,m.country,m.name) FROM Manufacturers m WHERE m.country = :county")
+    List<ManufacturerDTO> findAllManufacturerDTOByCountry(@Param("country") String country);
 }

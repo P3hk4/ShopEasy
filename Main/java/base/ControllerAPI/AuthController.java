0b0ec74c5e279c686f1f2e33efaa6fa123e89fee
Pavel Tarasov <62915355+P3hk4@users.noreply.github.com>
2025-05-09 19:06:43 +0400

    Registration page commit

--- Main/java/base/ControllerAPI/AuthController.java
+++ Main/java/base/ControllerAPI/AuthController.java
@@ -12,10 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -60,9 +57,10 @@ public class AuthController {
         ));
     }
 
-    @PostMapping("/register")
+    @PostMapping("/registration")
     @PreAuthorize("permitAll()")
     public ResponseEntity<String> register(@RequestBody Client client) {
+        System.out.println(client);
         clientService.saveClient(client);
        return ResponseEntity.ok("User registered successfully");
     }
